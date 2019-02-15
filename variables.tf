eaaf183073501d6b985ce36db066340929e68643
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2019-02-15 10:11:18 +0200

    format tf code

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "access_key" {
-  description = "AWS provider access key"  
+  description = "AWS provider access key"
 }
 
 variable "secret_key" {
-  description = "AWS provider secret key"  
+  description = "AWS provider secret key"
 }
 
 variable "region" {
-  default = "us-east-2"
+  default     = "us-east-2"
   description = "AWS region"
 }
 
 variable "ami" {
-  description = "AWS AMI id"  
+  description = "AWS AMI id"
 }
 
 variable "instance_type" {
-  description = "AWS instance type"  
+  description = "AWS instance type"
 }
 
 variable "subnet_id" {
@@ -24,10 +24,10 @@ variable "subnet_id" {
 }
 
 variable "public_key" {
-   description = "A public key for resource aws_key_pair that generates a key pair on AWS for the instance"
+  description = "A public key for resource aws_key_pair that generates a key pair on AWS for the instance"
 }
 
 variable "vpc_security_group_ids" {
-  type = "list"
+  type        = "list"
   description = "AWS VPC security group ids"
 }
